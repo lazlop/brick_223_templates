4cbeac9995861b25ada34286678485259c851f70
Lazlo <lazlopaul17@gmail.com>
2025-04-16 23:19:36 -0700

    cleaning

--- template_builder/get_completion.py
+++ template_builder/get_completion.py
@@ -4,10 +4,11 @@ import openai # CBORG API Proxy Server is OpenAI-compatible through the openai m
 import yaml
 with open('/Users/lazlopaul/Desktop/cborg/api_key.yaml', 'r') as file:
     API_KEY= yaml.safe_load(file)['key']
+    BASE_URL = yaml.safe_load(file)['base_url']
 
 client = openai.OpenAI(
     api_key=API_KEY,
-    base_url="https://api.cborg.lbl.gov" # Local clients can also use https://api-local.cborg.lbl.gov
+    base_url=BASE_URL
 )
 
 def get_completion(prompt, system_prompt = None):
@@ -27,7 +28,7 @@ def get_completion(prompt, system_prompt = None):
     response = client.chat.completions.create(
             model=MODEL, 
             messages = messages,
-            temperature=0.0   # Optional: set model temperature to control amount of variance in response
+            temperature=0.0  
         )
 
     return response.choices[0].message.content
